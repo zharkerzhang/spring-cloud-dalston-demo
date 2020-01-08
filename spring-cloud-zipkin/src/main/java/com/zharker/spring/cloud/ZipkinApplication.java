71b6483d3229a4b7b0722b032b5dd8827ae69fda
ezhnjrr <jerry.a.zhang@ericsson.com>
2020-01-08 17:27:56 +0800

    chore(Others): fullfill spring-cloud applications trace demo
    Change-Id: I4fa71fda3b5ab0a33da50edcef6c8a71c2bd835c

--- spring-cloud-zipkin/src/main/java/com/zharker/spring/cloud/ZipkinApplication.java
+++ spring-cloud-zipkin/src/main/java/com/zharker/spring/cloud/ZipkinApplication.java
@@ -2,10 +2,12 @@ package com.zharker.spring.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import zipkin.server.EnableZipkinServer;
 
 @EnableZipkinServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ZipkinApplication {
 
     public static void main(String[] args) {
